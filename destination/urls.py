e82295d97d5b632649d45a1833b9e0b141d9cf0b
GoRoS <olsngrs@gmail.com>
2021-03-29 21:16:32 +0300

    add basic views and urls

--- destination/urls.py
+++ destination/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-urlpatterns = [
+from .views import ListCityView, CityView, PointView
 
+urlpatterns = [
+    path('cities/', ListCityView.as_view(), name='cities'),
+    path('cities/<int:pk>', CityView.as_view(), name='city-details'),
+    path('points/<int:pk>', PointView.as_view(), name='point-details'),
 ]
--- destination/views.py
+++ destination/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import City, Point
+from .serializers import PointSerializer, CitySerializer, ListCitySerializer
 
+
+class CityView(generics.RetrieveAPIView):
+    serializer_class = CitySerializer
+    queryset = City.objects.all()
+
+
+class ListCityView(generics.ListAPIView):
+    serializer_class = ListCitySerializer
+    queryset = City.objects.all()
+
+
+class PointView(generics.RetrieveAPIView):
+    serializer_class = PointSerializer
+    queryset = Point.objects.all()
 
