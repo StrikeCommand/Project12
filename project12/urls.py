d8ee84448d5b588701d4ee86c3a87bc8d6fb3e54
GoRoS <olsngrs@gmail.com>
2021-03-29 21:15:57 +0300

    refactoring

--- project12/urls.py
+++ project12/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('destination/', include('destination.urls'))
+    path('api/', include('destination.urls'))
 ]
